553c1ce6d79d5bb9f766a46cfedc59bdea3a0273
Arkadiusz Kusiak <akusiak23@gmail.com>
2019-03-04 17:04:13 -0600

    dft_rec

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -98,3 +98,32 @@ class Graph:
                 # Then, put all of it's children into the stack
                 for neighbor in self.vertices[v]:
                     s.push(neighbor)
+
+    def dft_rec(self, v, visited=None):
+        if visited is None:
+            visited = []
+        if v not in visited:
+            visited.append(v)
+        for neighbor in self.vertices[v]:
+            self.dft_rec(neighbor, visited)
+
+
+graph = Graph()  # Instantiate your graph
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_vertex('4')
+graph.add_vertex('5')
+graph.add_vertex('6')
+graph.add_vertex('7')
+graph.add_directed_edge('5', '3')
+graph.add_directed_edge('6', '3')
+graph.add_directed_edge('7', '1')
+graph.add_directed_edge('4', '7')
+graph.add_directed_edge('1', '2')
+graph.add_directed_edge('7', '6')
+graph.add_directed_edge('2', '4')
+graph.add_directed_edge('3', '5')
+graph.add_directed_edge('2', '3')
+graph.add_directed_edge('4', '6')
+print(graph.vertices)
