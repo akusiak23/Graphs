6b6c693ad580711e31d2e9d6fc39417e492b022d
Arkadiusz Kusiak <akusiak23@gmail.com>
2019-03-04 15:22:10 -0600

    Initial commit

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -2,8 +2,11 @@
 Simple graph implementation
 """
 
+# initial commit
+
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
     def __init__(self):
         pass  # TODO
